31f366a5b59bda910a427ba727ba7dda74fc0924
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2022-02-04 22:02:26 +0100

    initial commit

---
+++ WPF_MVVM_Reservoir/Models/Hotel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_Reservoir.Models
+{
+    internal class Hotel
+    {
+        private readonly ReservationBook _reservationBook;
+        public string Name { get; }
+
+        public Hotel(string pName)
+        {
+            Name = pName;
+
+            _reservationBook = new ReservationBook();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        /// 
+        public IEnumerable<Reservation> GetReservationsForUser(string username)
+        { 
+            return _reservationBook.GetReservationsForUser(username);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reservation"></param>
+        /// <exception cref="ReservationConflictException"
+        public void MakeReservation(Reservation reservation)
+        { 
+            _reservationBook.AddReservation(reservation);
+        }
+    }
+}
---
+++ WPF_MVVM_Reservoir/Models/ReservationBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_MVVM_Reservoir.Exceptions;
+
+namespace WPF_MVVM_Reservoir.Models
+{
+    internal class ReservationBook
+    {
+        private readonly List<Reservation> _reservations;
+
+        public ReservationBook()
+        {
+            _reservations = new List<Reservation>();
+        }
+        public IEnumerable<Reservation> GetReservationsForUser(string username)
+        {
+            return _reservations.Where(r => r.UserName == username);
+        }
+        public void AddReservation(Reservation reservation)
+        {
+            foreach (Reservation existingReservation in _reservations)
+            {
+                if(existingReservation.Conflicts(reservation))
+                {
+                    throw new ReservationConflictException( existingReservation, reservation);
+                }
+            }
+            _reservations.Add(reservation);
+        }
+    }
+}
